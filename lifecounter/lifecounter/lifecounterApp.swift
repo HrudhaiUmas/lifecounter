e11612e40bb65ee9d8fe57e15b37a4afe2853b32
HrudhaiUmas <hrudhaiumas20@gmail.com>
2026-01-28 02:26:00 -0800

    Assignment doneeee

---
+++ lifecounter/lifecounter/lifecounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterApp.swift
+//  lifecounter
+//
+//  Created by Hrudhai Umas on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct lifecounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
